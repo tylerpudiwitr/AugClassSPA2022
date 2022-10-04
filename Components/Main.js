a440ae3121567865d0b822144bba824d271f24bf
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-03 20:55:05 -0500

    SPA

--- Components/Main.js
+++ Components/Main.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 import * as views from "./views";
 
-export default state => html`
-  ${views["Home"]()}, ${views["Bio"]()}, ${views["Pizza"]()},
-  ${views["Order"]()}
-`;
+export default state =>
+  html`
+    ${views[state.view](state)}
+  `;
