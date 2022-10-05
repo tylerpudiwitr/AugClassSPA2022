5e271c3b9d9ad1c6bbf5ceb82616361580f2a9c5
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-04 21:39:33 -0500

    API

--- server/app.js
+++ server/app.js
@@ -1,7 +1,18 @@
 // 'Import' the Express module instead of http
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
 // Initialize the Express application
 const app = express();
+
+const logging = (request, response, next) => {
+  console.log(`${request.method} ${request.url} ${Date.now()}`);
+  next();
+};
+
+app.use(express.json());
+app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
@@ -16,6 +27,8 @@ app.get("/echo/:input", (request, response) => {
   response.status(418).json({ echo: message });
 });
 
+const PORT = process.env.PORT || 4040; // we use || to provide a default value
+
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
-app.listen(4040, () => console.log("Listening on port 4040"));
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
