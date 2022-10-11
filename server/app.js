69659c447aca9c3cbc903d09327624b247422e32
tylerpudiwitr <tyler.pudiwitr@outlook.com>
2022-10-10 20:20:25 -0500

    spa

--- server/app.js
+++ server/app.js
@@ -1,16 +1,44 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
+
 // Initialize the Express application
 const app = express();
+
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
+// CORS Middleware
+const cors = (req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Accept,Authorization,Origin"
+  );
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+};
 
 const logging = (request, response, next) => {
   console.log(`${request.method} ${request.url} ${Date.now()}`);
   next();
 };
 
+app.use(cors);
 app.use(express.json());
 app.use(logging);
 
@@ -26,6 +54,8 @@ app.get("/echo/:input", (request, response) => {
   const message = request.params.input;
   response.status(418).json({ echo: message });
 });
+
+app.use(`/pizzas`, pizzas);
 
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
 
